b79bde786be90eac8b5c73575d31a9d15b8356f7
dongjubak <xstu6825@naver.com>
2023-06-15 19:35:11 +0900

    solved / lv1 / SQL고득점kit 3문제 / 20230615목

--- 01 프로그래머스/고득점kit_SQL/01 SELECT.sql
+++ 01 프로그래머스/고득점kit_SQL/01 SELECT.sql
@@ -1,4 +1,4 @@
-### 조건에 부합하는 중고거래 댓글 조회하기 (난이도1)
+### 조건에 부합하는 중고거래 댓글 조회하기 (난이도1) 2023.06.14
 # https://school.programmers.co.kr/learn/courses/30/lessons/164673
 # 성공 답안1
 SELECT
@@ -27,7 +27,7 @@ order by r.CREATED_DATE ,b.TITLE;
 
 
 
-### 강원도에 위치한 생산공장 목록 출력하기 (난이도1)
+### 강원도에 위치한 생산공장 목록 출력하기 (난이도1) 2023.06.14
 # https://school.programmers.co.kr/learn/courses/30/lessons/131112
 # 성공 답안1
 SELECT
@@ -39,10 +39,47 @@ WHERE ADDRESS like '강원도%';
 
 
 
-### 흉부외과 또는 일반외과 의사 목록 출력하기 (난이도1)
+### 흉부외과 또는 일반외과 의사 목록 출력하기 (난이도1) 2023.06.14
 # https://school.programmers.co.kr/learn/courses/30/lessons/132203
 # 성공 답안1
 SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') as HIRE_YMD
     FROM DOCTOR
     WHERE MCDP_CD IN ('CS','GS')
     ORDER BY HIRE_YMD DESC, DR_NAME ASC;
+
+
+
+### 조건에 맞는 도서 리스트 출력하기 (난이도1) 2023.06.15
+# https://school.programmers.co.kr/learn/courses/30/lessons/144853
+# 성공 답안1
+SELECT BOOK_ID, DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE
+    FROM BOOK
+    WHERE CATEGORY = '인문'
+    AND PUBLISHED_DATE LIKE '2021%'
+    ORDER BY PUBLISHED_DATE;
+
+
+
+### 평균 일일 대여 요금 구하기 (난이도1) 2023.06.15
+# https://school.programmers.co.kr/learn/courses/30/lessons/151136
+# 성공 답안1 : format으로 하면 ex.) 35,665 처럼 나와서 실패함. round로 해야 한다.
+SELECT round(avg(DAILY_FEE), 0) AS AVERAGE_FEE
+    FROM CAR_RENTAL_COMPANY_CAR
+    WHERE CAR_TYPE = 'SUV';
+
+
+
+### 과일로 만든 아이스크림 고르기 2023.06.15
+# https://school.programmers.co.kr/learn/courses/30/lessons/133025
+# 실패 답안1 : WHERE로만 하면 안됨 JOIN도 사용해야 함
+SELECT F.FLAVOR
+    FROM ICECREAM_INFO AS I, FIRST_HALF AS F
+    WHERE F.TOTAL_ORDER > 3000
+    AND I.INGREDIENT_TYPE = 'fruit_based';
+# 성공 답안 1
+SELECT F.FLAVOR
+    FROM ICECREAM_INFO AS I
+    LEFT JOIN FIRST_HALF AS F
+    ON I.FLAVOR = F.FLAVOR
+    WHERE F.TOTAL_ORDER > 3000
+    AND I.INGREDIENT_TYPE = 'fruit_based';
