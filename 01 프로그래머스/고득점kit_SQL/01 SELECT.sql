f5fb269c980bee317cd4dae5ff9dd853a3fcb863
dongjubak <xstu6825@naver.com>
2023-07-10 21:07:10 +0900

    solved / lv1 / SQL고득점kit 1문제 / 20230710월

--- 01 프로그래머스/고득점kit_SQL/01 SELECT.sql
+++ 01 프로그래머스/고득점kit_SQL/01 SELECT.sql
@@ -113,3 +113,11 @@ SELECT FLAVOR
 
 
 
+### 조건에 맞는 회원수 구하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/131535
+# 성공답안
+SELECT COUNT(USER_ID) AS USERS
+    FROM USER_INFO
+    WHERE AGE >= 20 AND AGE <= 29
+    AND joined like '2021%'
+
